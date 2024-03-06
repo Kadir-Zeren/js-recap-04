405779a8275a6dd8d85a9ad7f787637af53a91b2
kadir-zeren <k4dzeren@gmail.com>
2024-03-06 13:05:18 +0300

    while

--- app.js
+++ app.js
@@ -12,9 +12,24 @@
 // }
 // console.log(`SUM: ${sum}`);
 
-const adet = prompt("kac adet sayi uretmek istersiniz:?");
+// const adet = prompt("kac adet sayi uretmek istersiniz:?");
 
-for (let sayac = 1; sayac <= adet; sayac++) {
-  const rasgeleSayi = Math.round(Math.random() * 100);
-  console.log(`${sayac}. sayiniz : ${rasgeleSayi}`);
+// for (let sayac = 1; sayac <= adet; sayac++) {
+//   const rasgeleSayi = Math.round(Math.random() * 100);
+//   console.log(`${sayac}. sayiniz : ${rasgeleSayi}`);
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// console.log("BYE");
+
+let not = prompt("Lutfen 0-100 arasinda bir not girniz:");
+
+while (not < 0 || not > 100) {
+  console.log("Not 0'dan kucuk veya 100'den buyuk olamaz");
+  not = prompt("Lutfen 0-100 arasinda bir not giriniz");
 }
+console.log("Notunuz:", not);
