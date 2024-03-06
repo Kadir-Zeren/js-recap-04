759d7045667b1c3f389712e6a57dc60dbb31f6c8
kadir-zeren <k4dzeren@gmail.com>
2024-03-06 13:09:28 +0300

    do-while

--- app.js
+++ app.js
@@ -26,10 +26,20 @@
 // }
 // console.log("BYE");
 
-let not = prompt("Lutfen 0-100 arasinda bir not girniz:");
+// let not = prompt("Lutfen 0-100 arasinda bir not girniz:");
 
-while (not < 0 || not > 100) {
-  console.log("Not 0'dan kucuk veya 100'den buyuk olamaz");
-  not = prompt("Lutfen 0-100 arasinda bir not giriniz");
-}
+// while (not < 0 || not > 100) {
+//   console.log("Not 0'dan kucuk veya 100'den buyuk olamaz");
+//   not = prompt("Lutfen 0-100 arasinda bir not giriniz");
+// }
+// console.log("Notunuz:", not);
+
+let not;
+do {
+  not = prompt("Lutfen 0-100 arasinda bir not giriniz:");
+  if (not < 0 || not > 100) {
+    alert("Not 0'dan kucuk veya 100'den buyuk olamaz");
+  }
+} while (not < 0 || not > 100);
+
 console.log("Notunuz:", not);
